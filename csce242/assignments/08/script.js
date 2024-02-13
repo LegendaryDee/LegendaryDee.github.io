ecf330073e934f521e1eaf3df28885c489ecd8b3
Demetrius Mack <demetriusomack00@gmail.com>
2024-02-13 08:46:13 -0500

    added index.html

--- csce242/assignments/08/script.js
+++ csce242/assignments/08/script.js
@@ -8,39 +8,10 @@ const toggleHamburger = () => {
     document.getElementById("hamburger").onclick = toggleHamburger;
   };
 
-  const showImages = () =>
-  {
-    let userCommand = document.getElementById("enter-command").value.toLowerCase().trim();
-
-    if(userCommand == "b")
-    {
-        document.getElementById("commandImages").src = "images/read.jpg";
-    }
-    else if (userCommand == "c")
-    {
-        document.getElementById("commandImages").src = "images/clown.jpg";
-    }
-    else if (userCommand == "p")
-    {
-        document.getElementById("commandImages").src = "images/birthday.jpg";
-    }
-    else if (userCommand == "r")
-    {
-        document.getElementById("commandImages").src = "images/rain.jpg";
-    }
-    else if (userCommand == "s")
-    {
-        document.getElementById("commandImages").src = "images/shovel.jpg";
-    }
-    else if (userCommand == "w")
-    {
-        document.getElementById("commandImages").src = "images/work.jpg";
-    }
-  }
   let rangevalue = document.getElementById("myRange");
   let setvalue = document.getElementById("rangevalue");
-  let imgchangevalue = document.getElementById("imgChange")
-  rangevalue.oninput = function()
+  let imgchangevalue = document.getElementById("imgChange");
+   rangevalue.oninput = () => 
   {
       let getvalue = rangevalue.value;
       setvalue.innerHTML = this.value;
@@ -76,61 +47,53 @@ const toggleHamburger = () => {
       {
           imgchangevalue.setAttribute("src","images/yoga8.jpg")
       }
+    }
 
+    const showImages = () =>
+  {
+    let userCommand = document.getElementById("command-textbox").value.toLowerCase().trim();
+
+    if(userCommand == "b")
+    {
+        document.getElementById("commandImages").src = "images/read.jpg";
+    }
+    else if (userCommand == "c")
+    {
+        document.getElementById("commandImages").src = "images/clown.jpg";
+    }
+    else if (userCommand == "p")
+    {
+        document.getElementById("commandImages").src = "images/birthday.jpg";
+    }
+    else if (userCommand == "r")
+    {
+        document.getElementById("commandImages").src = "images/rain.jpg";
+    }
+    else if (userCommand == "s")
+    {
+        document.getElementById("commandImages").src = "images/shovel.jpg";
+    }
+    else if (userCommand == "w")
+    {
+        document.getElementById("commandImages").src = "images/work.jpg";
+    }
+  }
+  var div = document.getElementById("ex1")
+  var display = 0;
+  const exercise01 = () =>
+  {
+    if (display == 1)
+        {
+            div.style.display = "block";
+            display = 0;
+        }
+        else
+        {
+            div.style.display = "none";
+            display = 1;
+        }
   }
 
-// const changeP = (e) => {
-//     e.preventDefault(); //don't go to another page
-//     document.getElementById("display").textContent = "Hello Demetrius";
-// };
-
-// const showExercise = () => {
-//     const color = document.getElementById("enter-command").value.toLowerCase().trim();
-//     const moodP = document.getElementById("mood");
-//     let mood = "undeterminable";
-
-//     // const showCommand = () => {
-//     //     const color = document.getElementById("enter-command").value.toLowerCase().trim();
-//     //     const moodP = document.getElementById("mood");
-//     //     let mood = "undeterminable";
-    
-//     //     if (color == "b") {
-//     //         document.getElementById("commandImages").src="..images/read.jpg";
-//     //     } else if (color == "c") {
-//     //         mood.src = "clown.jpg";
-//     //     } else if (color == "p") {
-//     //         mood.src = "birthday.jpg";
-//     //     }
-//     //     else if (color == "r") {}
-    
-//     //     moodP.innerHTML = `You are in a ${mood}`;
-//     // }
-    
-    // const evalForm = (e) => {
-    //     e.preventDefault();
-    //     console.log("We did it!!");
-    // };
-
-    
-    
-    // function changePic() {   
-    //     if( myPic.src === "a.png") {       
-    //        myPic.src = "b.png";  
-    //      }  else {     
-    //         myPic.src = "a.png"; 
-    //      } 
-    //     }     
-//     /*Slider*/
-//     var slider = document.getElementById("myRange");
-//     var output = document.getElementById("demo");
-//     output.innerHTML=slider.value;
-
-//     slider.oninput = function()
-//     {
-//         output.innerHTML = this.value;
-//     }
-
-// }
-document.getElementById("enter-command").onkeyup = showImages;
-document.getElementById("form").onsubmit= evalForm;
-document.getElementById("link-click").onclick = changeP;
+  
+document.getElementById("command-textbox").onkeyup = showImages;
+// document.getElementById("value").
---
+++ csce242/projects/part4/script.js
@@ -0,0 +1,69 @@
+// Toggles the nav items to show/hide when the hamburger menu is clicked
+const toggleHamburger = () => {
+    document.getElementById("nav-items").classList.toggle("hide");
+  };
+  
+  // Calls the toggleHamburger function when the hamburger menu is clicked
+  window.onload = () => {
+    document.getElementById("hamburger").onclick = toggleHamburger;
+  };
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+const updateThermomether = () => {
+    const goal = 10000;
+    const number = document.getElementById("txt-number").value;
+    const errorSpan = document.getElementById("number-error");
+    errorSpan.innerHTML = "";
+    const therm = document.getElementById("thermometer");
+    document.querySelector(":root").style.setProperty('--number', "0%");
+
+    /* Error */
+    if (isNaN(number)) {
+        errorSpan.innerHTML = "* Not a valid number";
+        return;
+    } else if (funds < 0) {
+        errorSpan.innerHTML = "* Negative number";
+        return;
+    }
+
+    const percent = number / goal * 100;
+    document.querySelector(":root").style.setProperty('--number', percent + "%");
+
+};
+
+const evalForm = (e) => {
+    e.preventDefault();
+
+
+};
+
+document.getElementById("btn-cont").onclick = updateThermomether;
+document.getElementById("my-form").onsubmit = evalForm;
