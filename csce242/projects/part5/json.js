16064d487f12bf53ce25460d3d1cbf712b082bda
Demetrius Mack <demetriusomack00@gmail.com>
2024-03-20 12:30:42 -0400

    added index.html

---
+++ csce242/projects/part5/json.js
@@ -0,0 +1,35 @@
+//This is for Page 5
+
+[
+    {
+        "_playlist": 1,
+        "name": "Dee's Vaporwave Playlist",
+        "genres": ["Vaporwave", "Afrobeats", "80's"],
+        "iframe_name": "https://music.apple.com/us/playlist/vaporwave-mix/pl.u-KVXBDGJFLjWgEo"
+        // "iframe": "<iframe allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/vaporwave-mix/pl.u-KVXBDGJFLjWgEo"></iframe>"
+
+    },
+
+    {
+        "_playlist": 2,
+        "name": "Jersey Club Mix Playlist",
+        "genres": ["Jersey Mix", "Hip-Hop"],
+        "iframe_name": "https://music.apple.com/us/playlist/i-aint-from-jersey-but/pl.u-GgA59DbuZjaDq4J"
+    },
+
+    {
+        "_playlist": 3,
+        "name": "Early Roadtrip Essential",
+        "genres": ["R&B", "Hip-Hop", "House"],
+        "iframe_name": "https://embed.music.apple.com/us/playlist/early-road-trip-essential/pl.u-xlyNqrWuJNL0dK?theme=light"
+    },
+
+    {
+        "_playlist": 4,
+        "name": "Adult Swim Lofi Hip-Hop Playlist",
+        "genres": ["Jazz", "Hip-Hop"],
+        "iframe_name": "https://music.apple.com/us/playlist/adult-swim-lofi-hip-hop/pl.u-2aoqXKYhGX75mz"
+    }
+
+
+]
