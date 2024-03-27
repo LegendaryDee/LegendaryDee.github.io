cf837c0733e57ff7afdb9c3083ab07f484dd6fae
Demetrius Mack <demetriusomack00@gmail.com>
2024-03-27 16:15:57 -0400

    added index.html

--- csce242/projects/part5/script.js
+++ csce242/projects/part5/script.js
@@ -1,14 +1,13 @@
 const getPlaylists = async() =>
 {
-    const url ="https://legendarydee.github.io/csce242/json/playlist-info.json";
-    
+    let playlists= await getPlaylists();
+    let pSection = document.getElementById("playlist-section");
 
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    }    catch(error){
-            console.log(error);
-        }
+    playlists.forEach(playlist => {
+       pSection.append(getPlaylistItem(playlist));
+    });
+
+    console.log(playlists);
     
 };
 
@@ -24,41 +23,89 @@ const showPlaylists = async() =>
     
 };
 
-const getPlaylistSection = (playlist) =>
-{
-    const playlist_section = document.createElement("section");
-    playlist_section.classList.add("playlist_section");
+// const getPlaylistSection = (playlist) =>
+// {
+//     const playlist_section = document.createElement("section");
+//     playlist_section.classList.add("playlist_section");
     
-    const pSection = document.createElement("section");
-    pSection.classList.add("p-section");
-    playlist_section.append(pSection);
+//     const pSection = document.createElement("section");
+//     pSection.classList.add("p-section");
+//     playlist_section.append(pSection);
 
-    const pText = document.createElement("section");
-    pText.classList.add("playlist-text");
-    pText.append(pText);
+//     const pText = document.createElement("section");
+//     pText.classList.add("playlist-text");
+//     pText.append(pText);
 
-    const playlist_id = document.createElement("h3");
-    playlist_id.innerHTML = playlist.id;
-    playlist_section.append(playlist_id);
+//     const playlist_id = document.createElement("h3");
+//     playlist_id.innerHTML = playlist.id;
+//     playlist_section.append(playlist_id);
 
-    const iframe = document.createElement("img");
-    iframe.src = "https://" + playlist.iframe;
-    playlist_section.append(iframe);
+//     const iframe = document.createElement("img");
+//     iframe.src = "https://" + playlist.iframe;
+//     playlist_section.append(iframe);
 
-    const genre = document.createElement("h4");
-    genre.innerHTML = playlist.genre;
-    pSection.append(genre);
+//     const genre = document.createElement("h4");
+//     genre.innerHTML = playlist.genre;
+//     pSection.append(genre);
 
-    const runtime = document.createElement("h4");
-    runtime.innerHTML = playlist.runtime;
-    pSection.append(runtime);
+//     const runtime = document.createElement("h4");
+//     runtime.innerHTML = playlist.runtime;
+//     pSection.append(runtime);
 
-    const numOfSongs = document.createElement("h4");
-    numOfSongs.innerHTML = playlist.numOfSongs;
-    pSection.append(numOfSongs);
+//     const numOfSongs = document.createElement("h4");
+//     numOfSongs.innerHTML = playlist.numOfSongs;
+//     pSection.append(numOfSongs);
 
 
-    return playlist_section;
+//     return playlist_section;
+// };
+
+
+
+
+
+const getPlaylistItem = (playlist) => {
+    let section = document.createElement("section");
+
+    
+    let h3 = document.createElement("h3");
+    h3.innerText = playlist.name;
+    section.append(h3);
+
+    let ul = document.createElement("ul");
+    section.append(ul);
+    ul.append(getLi(playlist.genres));
+    ul.append(getLi(playlist.run-time));
+    ul.append(getLi(playlist.numOfSongs));
+    ul.append(getLi(playlist.features));
+
+    let iframe = document.createElement("img");
+    section.append(iframe);
+    iframe.src= "https://legendarydee.github.io/cscse242/json/images/playlist-section/" + house.main_image;
+    
+    
+    section.append(getFeatures(playlist.features));
+    // section.append(getImage(house.main_image));
+    return section;
+}
+
+const getLi = data => {
+    const li = document.createElement("li");
+    li.textContent = data;
+    return li;
 };
+
+
+const getFeatures = (features) => {
+    const section = document.createElement("section");
+
+    features.forEach(features => {
+        const p = document.createElement("p");
+        p.textContent = features;
+        section.append(p);
+    })
+    return section;
+}
+
 
 window.onload = () => showPlaylists();
